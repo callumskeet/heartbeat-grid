9a6c23588df54035d66c675089e4a7bb778408e4
Callum Skeet <callum@elitemail.org>
2022-11-13 20:37:15 +1100

    init

---
+++ HeartbeatGrid/HeartbeatGridView.h
@@ -0,0 +1,12 @@
+//
+//  HeartbeatGridView.h
+//  HeartbeatGrid
+//
+//  Created by Callum A Skeet on 13/11/2022.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface HeartbeatGridView : ScreenSaverView
+
+@end
---
+++ HeartbeatGrid/HeartbeatGridView.m
@@ -0,0 +1,51 @@
+//
+//  HeartbeatGridView.m
+//  HeartbeatGrid
+//
+//  Created by Callum A Skeet on 13/11/2022.
+//
+
+#import "HeartbeatGridView.h"
+
+@implementation HeartbeatGridView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
